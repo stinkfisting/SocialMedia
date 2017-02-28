abf6393552b9d67ef5b20368ac0985f02409e8de
Marcus Tam <Maynard@Marcuss-MacBook-Pro-3.local>
2017-02-28 15:56:42 -0500

    FirstCommit

---
+++ SocialMedia/SigninVC.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SocialMedia
+//
+//  Created by Marcus Tam on 2/28/17.
+//  Copyright © 2017 Marcus Tam. All rights reserved.
+//
+
+import UIKit
+
+class SigninVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
