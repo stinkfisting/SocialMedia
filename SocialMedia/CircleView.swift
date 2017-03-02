975a25f8b5b687928913d7522d02d8f091475fef
Marcus Tam <Maynard@Marcuss-MacBook-Pro-3.local>
2017-03-02 14:26:24 -0500

    feed UI complete

--- SocialMedia/CircleView.swift
+++ SocialMedia/CircleView.swift
@@ -1,16 +1,15 @@
 //
-//  FancyView.swift
+//  CircleView.swift
 //  SocialMedia
 //
-//  Created by Marcus Tam on 2/28/17.
+//  Created by Marcus Tam on 3/2/17.
 //  Copyright © 2017 Marcus Tam. All rights reserved.
 //
 
 import UIKit
 
-class FancyView: UIView {
-
-
+class CircleView: UIImageView {
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         
@@ -18,7 +17,13 @@ class FancyView: UIView {
         layer.shadowOpacity = 0.8
         layer.shadowRadius = 5.0
         layer.shadowOffset = CGSize(width: 1.0, height: 1.0)
-        layer.cornerRadius = 2.0
-        
     }
+    
+    
+    override func draw(_ rect: CGRect) {
+        super.draw(rect)
+        layer.cornerRadius = self.frame.width / 2
+
+    }
+
 }
