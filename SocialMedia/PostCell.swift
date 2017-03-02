3c949ee41fa37531416167be1480e3783bfc297e
Marcus Tam <Maynard@Marcuss-MacBook-Pro-3.local>
2017-03-02 16:08:03 -0500

    Feed-Table done

---
+++ SocialMedia/PostCell.swift
@@ -0,0 +1,27 @@
+//
+//  PostCell.swift
+//  SocialMedia
+//
+//  Created by Marcus Tam on 3/2/17.
+//  Copyright © 2017 Marcus Tam. All rights reserved.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+
+    @IBOutlet weak var profileImg: UIImageView!
+    @IBOutlet weak var userNameLbl: UILabel!
+    @IBOutlet weak var postImg: UIImageView!
+    @IBOutlet weak var caption: UITextView!
+    @IBOutlet weak var likesLbl: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+  
+    
+
+}
