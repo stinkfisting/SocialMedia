c8b50af48e3d7dee539df922f622bade58ce2d97
Marcus Tam <Maynard@Marcuss-MacBook-Pro-3.local>
2017-03-03 17:40:47 -0500

    Configure cell completed

--- SocialMedia/FeedVC.swift
+++ SocialMedia/FeedVC.swift
@@ -52,9 +52,13 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let post = posts[indexPath.row]
-        print("Miz: \(post.caption)")
         
-        return tableView.dequeueReusableCell(withIdentifier: "PostCell") as! PostCell
+        if let cell = tableView.dequeueReusableCell(withIdentifier: "PostCell") as? PostCell {
+            cell.configureCell(post: post)
+            return cell
+        } else {
+            return PostCell()
+        }
     }
     
     @IBAction func signOutTapped(_ sender: Any) {
